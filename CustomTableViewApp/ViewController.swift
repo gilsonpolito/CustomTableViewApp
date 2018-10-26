3836743792492f076f468e922e25d4056a18b9f1
Gilson Leandro Polito <gilsonpolito@gmail.com>
2018-10-26 20:40:26 -0300

    Initial Commit

---
+++ CustomTableViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomTableViewApp
+//
+//  Created by pos on 26/10/2018.
+//  Copyright © 2018 pos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
